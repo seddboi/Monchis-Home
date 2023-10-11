69053006e3c13f3f4327bcc6ff68c25ece7b41d8
Gian Zamora <gz2016@att.net>
2023-10-11 00:58:38 -0700

    added custom background from figma

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -3,9 +3,9 @@ import { Header } from './header';
 
 export function Layout({ children }) {
 	return (
-		<>
+		<div className="bg-[url('/butterflies.png')] bg-center">
 			<Header />
 			<main>{children}</main>
-		</>
+		</div>
 	);
 }
